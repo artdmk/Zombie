c353a08685989fda48bdd341a5fd62267f9b109c
artdmk <artdmk@icloud.com>
2016-04-24 10:49:02 +0300

    Initial Commit

---
+++ Zombie/GameScene.swift
@@ -0,0 +1,116 @@
+//
+//  GameScene.swift
+//  Zombie
+//
+//  Created by Artem Demchenko on 06.04.16.
+//  Copyright (c) 2016 artdmk. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class Zombie:SKSpriteNode {
+    let mySprite = SKSpriteNode(imageNamed: "zombie1")
+}
+class GameScene: SKScene {
+    
+    let movePositionsPerSecond:CGFloat = 3.0
+    var velocity = CGPoint(x: 1, y: 1)
+//    let mySprite = SKSpriteNode(color: UIColor(colorLiteralRed: 200, green: 70, blue: 0, alpha: 1.0) , size: CGSize(width: 10, height: 10))
+    
+    let mySprite = SKSpriteNode(imageNamed: "zombie1")
+  
+    
+    override func didMoveToView(view: SKView) {
+        backgroundColor = SKColor.blackColor()
+        let background = SKSpriteNode(imageNamed: "background1")
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        background.size = CGSize(width: size.width, height: size.height)
+        background.zPosition = -1
+        addChild(background)
+       
+        mySprite.position = CGPoint(x: 200, y: 200)
+        mySprite.setScale(0.5)
+        addChild(mySprite)
+        
+        
+    }
+    
+    override func update(currentTime: NSTimeInterval) {
+        
+        func moveSprite(sprite:SKSpriteNode, velocity:CGPoint){
+            let amountToMove = CGPoint(x:velocity.x, y:velocity.y)
+            sprite.position += amountToMove}
+        
+        moveSprite(mySprite, velocity: velocity)
+        checkBounds()
+        rotateSprite(mySprite, direction: velocity)
+        
+    }
+    
+    func rotateSprite(sprite:SKSpriteNode, direction:CGPoint){
+        sprite.zRotation = direction.angle
+    }
+    
+    func moveSpriteToward(location:CGPoint){
+        let offset = location-mySprite.position
+        let direction = offset.normalized()
+        velocity = direction*movePositionsPerSecond
+    }
+    
+    func sceneTouched(touchLocation:CGPoint){
+        moveSpriteToward(touchLocation)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        
+        let touchLocation = touch.locationInNode(self)
+        sceneTouched(touchLocation)
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        guard let touch = touches.first else{
+            return
+        }
+        
+        let touchLocation = touch.locationInNode(self)
+        sceneTouched(touchLocation)
+    }
+    
+    func checkBounds(){
+        let bottomleft = CGPointZero
+        let topRight = CGPoint(x:size.width, y:size.height)
+        
+        if mySprite.position.x <= bottomleft.x{
+            mySprite.position.x = bottomleft.x
+            velocity.x = -velocity.x
+//            playSound("changeDirection.wav")
+        }
+        if mySprite.position.x >= topRight.x{
+            mySprite.position.x = topRight.x
+            velocity.x = -velocity.x
+//            playSound("changeDirection.wav")
+        }
+        if mySprite.position.y <= bottomleft.y{
+            mySprite.position.y = bottomleft.y
+            velocity.y = -velocity.y
+//            playSound("changeDirection.wav")
+        }
+        if mySprite.position.y >= topRight.y{
+            mySprite.position.y = topRight.y
+            velocity.y = -velocity.y
+//            playSound("changeDirection.wav")
+        }
+        
+    }
+    
+    func playSound(sound:String){
+        
+        runAction(SKAction.playSoundFileNamed(sound, waitForCompletion: false))
+        
+    }
+    
+}
---
+++ Zombie/GameViewController.swift
@@ -0,0 +1,54 @@
+//
+//  GameViewController.swift
+//  Zombie
+//
+//  Created by Artem Demchenko on 06.04.16.
+//  Copyright (c) 2016 artdmk. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Configure the view.
+        let skView = self.view as! SKView
+        let scene = GameScene(size: skView.bounds.size)
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+       
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
+        
+        /* Set the scale mode to scale to fit the window */
+        scene.scaleMode = .AspectFill
+        
+        skView.presentScene(scene)
+    
+    }
+
+//    override func shouldAutorotate() -> Bool {
+//        return true
+//    }
+//
+//    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+//        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+//            return .AllButUpsideDown
+//        } else {
+//            return .All
+//        }
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Release any cached data, images, etc that aren't in use.
+//    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
